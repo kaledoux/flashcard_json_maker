11313f03beecd2582feed5016e3ea3f0e320fbcf
Kyle Ledoux <kledoux@spreedly.com>
2022-03-01 11:33:36 -0500

    module cleanup and basic test

--- lib/flashcard_json_maker.ex
+++ lib/flashcard_json_maker.ex
@@ -1,47 +1,28 @@
 defmodule FlashcardJsonMaker do
-  import File
   @keys {"kanji", "fronttext", "backtext", "source", "page"}
   @moduledoc """
   Documentation for `FlashcardJsonMaker`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FlashcardJsonMaker.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+@doc "transformation pipeline that does the following:
+  reads the source file as a stream
+  takes each line and parses by csv
+  formats the parsed list into a json formatted string
+  ... should append each item to a target json file"
 
   def process_csv do
     read_csv_source()
       |> Stream.map(&String.trim(&1))
       |> Stream.map(&String.split(&1, ","))
-      |> Stream.map(&format_csv_row_as_json_object(&1))
+      |> Stream.map(&format_csv_row_as_json_string(&1))
       |> Enum.each(&IO.puts(&1))
   end
 
-# recurse with counter to track index position in csv
-# when counter is at final index end recurse
-# object construction method uses index to place value in obj
-# this requires extra object passed in as well
-
-# just got compile through, need to test format_csv_row function
-  def format_csv_row_as_json_object(csv_row) do
-    flashcard = empty_flashcard_json()
-    format_row(csv_row, flashcard, 0)
+  def format_csv_row_as_json_string(csv_row) do
+    format_row(csv_row, %{}, 0)
   end
 
   def format_row([], flashcard, _), do: map_to_json(flashcard)
-
-  def map_to_json(map) do
-    "{\n  \"kanji\": #{map["kanji"]},\n  \"fronttext\": \"#{map["fronttext"]}\",\n  \"backtext\": \"#{map["backtext"]}\",\n  \"source\": \"#{map["source"]}\",\n  \"page\": #{map["page"]}\n}"
-  end
 
   def format_row([current | remaining], flashcard, index) do
     updated_flashcard_map = Map.put(flashcard, elem(@keys, index), current)
@@ -49,15 +30,14 @@ defmodule FlashcardJsonMaker do
     format_row(remaining, updated_flashcard_map, index + 1)
   end
 
-  defp empty_flashcard_json, do: %{"kanji" => nil, "fronttext" => nil, "backtext" => nil, "source" => nil, "page" => nil}
-  
+  def map_to_json(map) do
+    "{\n  \"kanji\": #{map["kanji"]},\n  \"fronttext\": \"#{map["fronttext"]}\",\n  \"backtext\": \"#{map["backtext"]}\",\n  \"source\": \"#{map["source"]}\",\n  \"page\": #{map["page"]}\n}"
+  end
+
   def read_csv_source do 
     File.stream!("./data/new_flashcards.csv") 
   end
 
-  def parse_csv_row do
-    Stream.map(&String.split(&1, ","))
-  end
   # General Requirements:
   # Take a csv file
   #   delimited by "," for fields and "\n" for entries
@@ -69,18 +49,10 @@ defmodule FlashcardJsonMaker do
   #   Write to file, appending, not replacing
 
   # RoadMap:
-  # Parsing
-  # Function that parses one line of csv and returns appropriate json object
-  # Function that read a csv file and parses each entry into a list of csv entries
-  # Function that takes csv file, parses into list, then applies json conversion to each to form a new list
-  #   - pipeline this
-
-  # Reading/Writing
-    # use streaming to pipeline for large csv efficiency
-  # Reading one file in a hardcoded location
   # Writing to a file in a hardcoded location
 
   # Reading one file in a specified location
   # Writing to a file in a hardcoded location
   #   - main function takes an arugment (file_location)
+  def keys, do: @keys
 end
--- test/flashcard_json_maker_test.exs
+++ test/flashcard_json_maker_test.exs
@@ -1,8 +1,19 @@
 defmodule FlashcardJsonMakerTest do
   use ExUnit.Case
+  import FlashcardJsonMaker
   doctest FlashcardJsonMaker
+  @sample_flashcard %{"kanji" => true, "fronttext" => "front text", "backtext" => "back text", "source" => "book1", "page" => 1}
+  @sample_flashcard_json "{\n  \"kanji\": true,\n  \"fronttext\": \"front text\",\n  \"backtext\": \"back text\",\n  \"source\": \"book1\",\n  \"page\": 1\n}"
 
-  test "greets the world" do
-    assert FlashcardJsonMaker.hello() == :world
+  test "keys module attribute contains proper keys" do
+    assert elem(keys(), 0) == "kanji"
+    assert elem(keys(), 1) == "fronttext"
+    assert elem(keys(), 2) == "backtext"
+    assert elem(keys(), 3) == "source"
+    assert elem(keys(), 4) == "page"
+  end
+
+  test "format row returns json string when passed empty list" do
+    assert format_row([], @sample_flashcard, 0) == @sample_flashcard_json
   end
 end
