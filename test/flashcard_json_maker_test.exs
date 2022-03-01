61363647ea010eb3e92c8cf0e18d92913bed51a2
Kyle Ledoux <kledoux@spreedly.com>
2022-03-01 11:42:29 -0500

    tests for parsing and processing functions

--- test/flashcard_json_maker_test.exs
+++ test/flashcard_json_maker_test.exs
@@ -3,6 +3,7 @@ defmodule FlashcardJsonMakerTest do
   import FlashcardJsonMaker
   doctest FlashcardJsonMaker
   @sample_flashcard %{"kanji" => true, "fronttext" => "front text", "backtext" => "back text", "source" => "book1", "page" => 1}
+  @sample_flashcard_list ["true", "front text", "back text", "book1", 1]
   @sample_flashcard_json "{\n  \"kanji\": true,\n  \"fronttext\": \"front text\",\n  \"backtext\": \"back text\",\n  \"source\": \"book1\",\n  \"page\": 1\n}"
 
   test "keys module attribute contains proper keys" do
@@ -15,5 +16,17 @@ defmodule FlashcardJsonMakerTest do
 
   test "format row returns json string when passed empty list" do
     assert format_row([], @sample_flashcard, 0) == @sample_flashcard_json
+  end
+  
+  test "format row returns json string when passed complete list" do
+    assert format_row(@sample_flashcard_list, %{}, 0) == @sample_flashcard_json
+  end
+  
+  test "map_to_json returns properly formatted json string" do
+    assert map_to_json(@sample_flashcard) == @sample_flashcard_json
+  end
+
+  test "format_row_as_json_string formats properly" do
+    assert format_csv_row_as_json_string(@sample_flashcard_list) == @sample_flashcard_json 
   end
 end
